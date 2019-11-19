de12c9a8b5c5731592b8e910a78a1d4cdbcd4a28
Mike North <michael.l.north@gmail.com>
2019-11-18 18:07:14 -0800

    include dist folder in package

--- src/commands/rcv.ts
+++ src/commands/rcv.ts
@@ -5,7 +5,7 @@ export const pipedData: [string, string, string][] = [];
 
 const program = commander;
 
-function ms([sec, nano]: [number, number]) {
+function ms([sec, nano]: [number, number]): number {
   return sec * 1000 + nano / 1000000;
 }
 
@@ -13,10 +13,10 @@ const beginTime = process.hrtime();
 if (!process.stdin.isTTY) {
   let lastTime = beginTime;
   process.stdin.on('readable', function(this: ReadStream) {
-    let chunk = this.read();
+    const chunk = this.read();
     if (chunk !== null) {
-      let time = process.hrtime(beginTime);
-      let dt = ms(time) - ms(lastTime);
+      const time = process.hrtime(beginTime);
+      const dt = ms(time) - ms(lastTime);
       lastTime = time;
       pipedData.push([ms(time).toFixed(2), dt.toFixed(2), chunk]);
     }
